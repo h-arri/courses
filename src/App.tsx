29c649734dd3f5866c18a18b18b9687dffcb0754
OEM Configuration (temporary user) <hari2309haran@gmail.com>
2020-10-21 16:58:33 +0200

    Project setup and install necessary libs

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hallo!
+      </header>
+    </div>
+  );
+}
+
+App.displayName = 'App';
+
+export default App;
