53724ef6447ed75e716c8c91292f9b10c0a8d9d1
Hari <hari2309haran@gmail.com>
2021-04-04 13:00:57 +0200

    Updates

---
+++ precache-manifest.82c348d6a531a752b8fc044f6bae7d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e223a85907410aab1996337f7a4cd8",
+    "url": "/horses/index.html"
+  },
+  {
+    "revision": "216b83f441760571c88b",
+    "url": "/horses/static/css/main.061644f4.chunk.css"
+  },
+  {
+    "revision": "8ca948a481eb234cc999",
+    "url": "/horses/static/js/2.cfa9b82a.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/horses/static/js/2.cfa9b82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216b83f441760571c88b",
+    "url": "/horses/static/js/main.95b220d2.chunk.js"
+  },
+  {
+    "revision": "6f0e4f6bf3e83f583227",
+    "url": "/horses/static/js/runtime-main.871970a9.js"
+  }
+]);
